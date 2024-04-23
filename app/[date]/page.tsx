f2a6820e1e54bb40abc48a160d1ca3795a95fa1b
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2024-04-23 13:31:35 +0900

    動画を追加

--- app/[date]/page.tsx
+++ app/[date]/page.tsx
@@ -17,12 +17,15 @@ export async function generateStaticParams(): Promise<paramsType[]> {
 const page = async ({ params }: { params: paramsType }) => {
     const data:any = await fsPromises.readFile("./app/data/blog.json");
     const jsonData = JSON.parse(data);
+    console.log(jsonData[params.date].videos)
     return (
         <div className="max-w-3xl mx-auto p-5">
           <h1 className="text-4xl text-center mb-10 mt-10">
             {params.date}
           </h1>
-          {/* relative を追加 */}
+          <div className="text-lg leading-relaxed text-justify">
+            <p>{jsonData[params.date].description}</p>
+          </div>
           {jsonData[params.date].images.length > 0 ? 
           jsonData[params.date].images.map((image:any) => {
             return <img key={image}
@@ -33,10 +36,17 @@ const page = async ({ params }: { params: paramsType }) => {
             className="mb-4"
           />
           }) : ""}
-
-          <div className="text-lg leading-relaxed text-justify">
-            <p>{jsonData[params.date].description}</p>
-          </div>
+          {jsonData[params.date].videos.length > 0 ? 
+          jsonData[params.date].videos.map((video:any) => {
+            return <iframe key={video}
+            style={{ width: '100%', aspectRatio: '16/9', border: '0' }}
+            src={video}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="mb-4"
+          />
+          }) : ""}
         </div>
       );
 };
